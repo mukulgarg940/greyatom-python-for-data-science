e08c004567f989e11a1f390f72c40dcebf3a9539
mukulgarg940 <mukulgarg940@users.noreply.github.com>
2020-05-09 19:37:14 +0530

    Code file created.

---
+++ Project1/code.py
@@ -0,0 +1,68 @@
+# --------------
+# Code starts here
+
+# Create the lists 
+class_1=['Geoffrey Hinton','Andrew Ng','Sebastian Raschka','Yoshua Bengio']
+class_2=['Hilary Mason','Carla Gentry','Corinna Cortes']
+# Concatenate both the strings
+new_class=class_1+class_2
+
+# Append the list
+new_class.append('Peter Warden')
+
+# Print updated list
+print(new_class)
+
+# Remove the element from the list
+new_class.remove('Carla Gentry')
+# Print the list
+print(new_class)
+
+
+# Create the Dictionary
+courses={'Math':65,'English':70,'History':80,'French':70,'Science':60}
+
+
+
+# Slice the dict and stores  the all subjects marks in variable
+
+
+# Store the all the subject in one variable `Total`
+
+# Print the total
+total=[]
+for ite in courses:
+    total.append(courses[ite])
+print(total)
+# Insert percentage formula
+percentage=sum(total)*0.2
+# Print the percentage
+print(percentage)
+
+
+
+# Create the Dictionary
+ 
+mathematics={'Geoffrey Hinton':78,'Andrew Ng':95,'Sebastian Raschka':65,'Yoshua Benjio':50,'Hilary Mason':70,'Corinna Cortes':66,'Peter Warden':75}
+
+
+# Given string
+maximum=max(list(mathematics.values()))
+for i in mathematics:
+    if mathematics[i]==maximum:
+        topper=i
+        break
+# Create variable first_name 
+first_name=topper.split(" ")[0]
+# Create variable Last_name and store last two element in the list
+last_name=topper.split(" ")[1]
+# Concatenate the string
+full_name=last_name + " " + first_name
+# print the full_name
+print(full_name)
+certificate_name=full_name.upper()
+# print the name in upper case
+print(certificate_name)
+# Code ends here
+
+
